af3e79fd2e76a4b789ebc9aa9fb21b06f86fc3fa
AOLebets <89778658+AOLebets@users.noreply.github.com>
2022-08-29 21:57:01 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,15 +4,15 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println( HomeworkEleven.findOddIndexedNames(new String[]{"Alex", "Bob", "Alexa", "Kris", "Dolla"}));
+       System.out.println( HomeworkEleven.findOddIndexedNames(new String[]{"Alex", "Bob", "Alexa", "Kris", "Dolla"}));
 
-        System.out.println( Arrays.toString( HomeworkEleven.returnFormattedUpperZtA( new String[]{"dgds", "safasf", "aldlfmao", "jsbdaoab"} ) ) );
+       System.out.println( Arrays.toString( HomeworkEleven.returnFormattedUpperZtA( new String[]{"dgds", "safasf", "aldlfmao", "jsbdaoab"} ) ) );
 
-        HomeworkEleven.numToSortedString(new String[]{"1, 2, 0", "4, 5"});
+       System.out.println(HomeworkEleven.numToSortedString(new String[]{"1, 2, 0", "4, 5"}));
 
-        HomeworkEleven.randomNumsGenerator(25214903917L, 11L, 2^48L).limit(20).forEach(System.out::println);
+       HomeworkEleven.randomNumsGenerator(25214903917L, 11L, 2^48L).limit(20).forEach(System.out::println);
 
-        Stream res = HomeworkEleven.zip(Stream.of("sf", "sdf", "sada"), Stream.of("rtre", "ytu", "utrr", "kirby"));
-        res.forEach(n -> System.out.println(n));
+       Stream<String> res = HomeworkEleven.zip(Stream.of("sf", "sdf", "sada"), Stream.of("rtre", "ytu", "utrr", "kirby"));
+       res.forEach(System.out::println);
     }
 }
